eb192a35ebfae7d2a0248f0fe524069eb222e924
WonderKingOp <WonderKingOp@users.noreply.github.com>
2024-01-27 00:39:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -145,7 +145,7 @@ async function displayAlbums() {
 
     Array.from(document.getElementsByClassName("card")).forEach(e => {
         e.addEventListener("click", async (item) => {
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
+            songs = await getSongs(`Songs/${item.currentTarget.dataset.folder}`)
             playMusic(songs[0])
 
         })
